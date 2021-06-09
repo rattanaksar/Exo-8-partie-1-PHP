bf615b10f21b6ce2c5967eafbd49c841c78f90b0
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 11:32:04 +0200

    Exo 8 PHP

---
+++ Exo 8.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 8 : Les variables PHP</title>
+</head>
+<body>
+    <h1>Exo 8 Partie : Les variables PHP</h1>
+    <?php
+       $x = 3 + 4;
+       $y = 5 * 20;
+       $a = 45 / 5;
+       echo $x;
+       echo "<br>";
+       echo $y;
+       echo "<br>";
+       echo $a;
+    ?>
+</body>
+</html>
